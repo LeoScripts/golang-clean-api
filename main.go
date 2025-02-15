cd3bb95aace15b98d83301b27745909ccb602b5e
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2025-02-14 21:03:39 -0300

    feat: find all

--- main.go
+++ main.go
@@ -6,12 +6,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Student struct {
+	ID       int    `json:"_id"`
+	FullName string `json:"full_name"`
+	Age      int    `json:"age"`
+}
+
+// mock
+var Students = []Student{
+	Student{1, "Alex", 38},
+	Student{2, "Bia", 32},
+	Student{3, "Carlos", 28},
+}
+
 func heartController(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Deu bommmmmmmm",
 	})
 
 	c.Done()
+}
+
+func routeGetStudents(c *gin.Context) {
+	c.JSON(http.StatusOK, Students)
 }
 
 func main() {
@@ -25,5 +42,8 @@ func main() {
 
 func getRoutes(c *gin.Engine) *gin.Engine {
 	c.GET("/heart", heartController)
+
+	groupStudents := c.Group("/students")
+	groupStudents.GET("/", routeGetStudents)
 	return c
 }
